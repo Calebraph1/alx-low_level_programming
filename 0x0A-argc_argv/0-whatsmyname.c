2eae153dd450006bd1a64899de62802e25a8a941
Calebraph1 <craphl16@gmail.com>
2023-04-07 06:00:42 -0700

    Argument to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+    printf("%d\n", argc - 1);
+    return (0);
+}
